a599d06086043d158c957f59f7e75ed413fe82dd
Amodhx <amxdhnanditha@gmail.com>
2024-05-14 10:42:25 +0530

    calculation done

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let values = [];
-let symbols = [];
 
 $(".btnValue").click(function () {
     let text = $(this).text();
@@ -18,38 +16,12 @@ $(".symbol").click(function () {
 
 $("#reset").on('click',()=>{
     $("#inputField").val("");
-    values = [];
-    symbols = [];
 })
 
 $("#assign").on('click',()=>{
     let val = $("#inputField").val();
-    values = val.split(/[+\-*\/]/);
-    const symbolsArray = ['+', '-', '*', '/'];
-
-
-    for (let i = 0; i < val.length; i++) {
-        if (symbolsArray.includes(val[i])) {
-            symbols.push(val[i]);
-        }
-    }
-
-    for (let i = 0; i < values.length - 1; i++) {
-        let value1 = values[i];
-        let value2 = values[i+1];
-        let symbol = symbols[i];
-        if (symbol == "+"){
-            let result = value1 + value2;
-            $("#inputField").val(result)
-
-        }else if (symbol == "-"){
-
-        }else if (symbol == "/"){
-
-        }else if (symbol == "*"){
-
-        }
-    }
+    let eval1 = eval(val);
+    $("#inputField").val(eval1);
 
 });
 
